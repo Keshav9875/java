961ad6cf2c6761477c9e408eb937117203b0a4c8
keshav Garg <garg68860@gmail.com>
2024-07-23 15:20:51 +0530

    Escapesequence in java

---
+++ escapesequence.java
@@ -0,0 +1,11 @@
+public class escapesequence {
+    public static void main(String[] args) {
+        System.out.println("hello\nworld");
+        System.out.println("hello\tworld");
+        System.out.println("hello\bworld");
+        System.out.println("hello\'world\'");
+        System.out.println("hello\"world\"");
+        System.out.println("c:\\users\\public");
+    }
+    
+}
